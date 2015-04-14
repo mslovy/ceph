cfd6124909f66d33e114c829e67fc95c997cf6ec
Xinze Chi <xmdxcxz@gmail.com>
2015-04-14 23:07:34 +0800

    osd: misc latency perf counter for tier pool
    l_osd_agent_flush_lat, l_osd_agent_promote_lat, l_osd_agent_proxy_read_lat
    
    Signed-off-by: Xinze Chi <xmdxcxz@gmail.com>
    (cherry picked from commit e54cb9eeb8e6ece60e89ef7f919de2a1bb5f1d54)

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -2149,6 +2149,11 @@ void OSD::create_logger()
   osd_plb.add_u64_counter(l_osd_object_ctx_cache_hit, "object_ctx_cache_hit");
   osd_plb.add_u64_counter(l_osd_object_ctx_cache_total, "object_ctx_cache_total");
 
+  osd_plb.add_u64_counter(l_osd_op_cache_hit, "op_cache_hit");
+  osd_plb.add_time_avg(l_osd_tier_flush_lat, "osd_tier_flush_lat");
+  osd_plb.add_time_avg(l_osd_tier_promote_lat, "osd_tier_promote_lat");
+  osd_plb.add_time_avg(l_osd_tier_r_lat, "osd_tier_r_lat");
+
   logger = osd_plb.create_perf_counters();
   cct->get_perfcounters_collection()->add(logger);
 }
